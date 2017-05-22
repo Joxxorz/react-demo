5c8bbf2a031dc0f2c482847a4b80674dfdcf3db6
Joxxorz <jockperkins@jocks-mbp.lsm.local>
2017-05-22 11:25:03 +0100

    * various changes including adding bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+var jock = require('react-jockbar');
+var $ = require('jquery');
 
 class App extends Component {
   render() {
+
+    var width = 0;
+    var height = 10;
+    var color = 'red';
+    var time = 5;
+    var jockBar = jock.progressBar(width, height, color, time);
+    $('#jock-bar').html(jockBar);
+
     return (
       <div className="App">
         <div className="App-header">
@@ -11,8 +21,9 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Test.
+          First bar
         </p>
+        <div id="jock-bar"></div>
       </div>
     );
   }
