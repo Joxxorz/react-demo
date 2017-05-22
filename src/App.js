35f4725b20ee141bf2ef5edb785366016ea702dd
Joxxorz <jockperkins@jocks-mbp.lsm.local>
2017-05-22 14:09:04 +0100

    * updated how the trigger works

--- src/App.js
+++ src/App.js
@@ -6,32 +6,60 @@ var $ = require('jquery');
 
 class App extends Component {
 
-  runTimer(){
-    jock.triggerBar();
+  runTimer(barId){
+    jock.triggerBar(barId);
   }
 
   render() {
-
-    var width = 0;
-    var height = 10;
-    var color = 'red';
-    var time = 5;
-    var jockBar = jock.progressBar(width, height, color, time);
+    var slowBar = jock.progressBar(0, 10, 'red', 10);
+    var quickBar = jock.progressBar(0, 10, 'blue', 2);
+    var ploadBar = jock.progressBar(0, 10, 'black', 5);
+    var tloadBar = jock.progressBar(30, 10, 'purple', 5);
 
     $(document).ready(function() {
-      $('#jock-bar').html(jockBar);
+      $('#slow-bar').html(slowBar);
+      $('#quick-bar').html(quickBar);
+      $('#pload-bar').html(ploadBar);
+      $('#tload-bar').html(tloadBar);
     });
 
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Jockbar</h2>
         </div>
-        <p className="App-intro">
-          text change
-        </p>
-        <div id="jock-bar"></div>
+
+        <div className="bar-type">
+          <p className="bar-copy">
+            This bar loaded on page load, in black.
+          </p>
+          <div id="pload-bar"></div>
+        </div>
+
+        <div className="bar-type">
+          <p className="bar-copy">
+            A slow bar, in red.
+          </p>
+          <div id="slow-bar"></div>
+          <button className="btn-bars" onClick={this.runTimer('slow-bar')}>Click Me</button>
+        </div>
+
+        <div className="bar-type">
+          <p className="bar-copy">
+            A slow bar, in blue.
+          </p>
+          <div id="quick-bar"></div>
+          <button className="btn-bars" onClick={this.runTimer('quick-bar')}>Click Me</button>
+        </div>
+
+        <div className="bar-type">
+          <p className="bar-copy">
+            A third loaded bar, in purple.
+          </p>
+          <div id="tload-bar"></div>
+          <button className="btn-bars" onClick={this.runTimer('tload-bar')}>Click Me</button>
+        </div>
       </div>
     );
   }
