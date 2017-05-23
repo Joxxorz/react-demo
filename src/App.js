2ba98f378f38a31826be13dc959dfc4716fad427
Joxxorz <jockperkins@jocks-mbp.lsm.local>
2017-05-23 09:29:19 +0100

    Final commit for release

--- src/App.js
+++ src/App.js
@@ -21,13 +21,25 @@ class App extends Component {
       $('#quick-bar').html(quickBar);
       $('#pload-bar').html(ploadBar);
       $('#tload-bar').html(tloadBar);
+
+
+      var checkFalse = 0;
+      do {
+        if($("#pload-bar .loading-bar").length != 0){
+          setTimeout(function(){
+            jock.triggerBar('pload-bar');
+          }, 2000);
+
+          checkFalse = 1;
+        }
+      }
+      while (checkFalse < 1);
     });
 
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Jockbar</h2>
+          <h2>React Jockbar Demo</h2>
         </div>
 
         <div className="bar-type">
@@ -37,28 +49,49 @@ class App extends Component {
           <div id="pload-bar"></div>
         </div>
 
+        <hr />
+
         <div className="bar-type">
           <p className="bar-copy">
             A slow bar, in red.
           </p>
           <div id="slow-bar"></div>
-          <button className="btn-bars" onClick={this.runTimer('slow-bar')}>Click Me</button>
+          <button className="btn-bars" onClick={() => {this.runTimer('slow-bar')}}>Click Me</button>
         </div>
+
+        <hr />
 
         <div className="bar-type">
           <p className="bar-copy">
-            A slow bar, in blue.
+            A quick bar, in blue.
           </p>
           <div id="quick-bar"></div>
-          <button className="btn-bars" onClick={this.runTimer('quick-bar')}>Click Me</button>
+          <button className="btn-bars" onClick={() => {this.runTimer('quick-bar')}}>Click Me</button>
         </div>
 
-        <div className="bar-type">
+        <hr />
+
+        <div className="bar-type last-bar">
           <p className="bar-copy">
             A third loaded bar, in purple.
           </p>
           <div id="tload-bar"></div>
-          <button className="btn-bars" onClick={this.runTimer('tload-bar')}>Click Me</button>
+          <button className="btn-bars" onClick={() => {this.runTimer('tload-bar')}}>Click Me</button>
+        </div>
+
+        <div className="App-footer">
+          <div className="footer-message">Buy me a coffee?</div>
+          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_donations" />
+            <input type="hidden" name="business" value="joxxorz@me.com" />
+            <input type="hidden" name="lc" value="SI" />
+            <input type="hidden" name="item_name" value="Jock Perkins" />
+            <input type="hidden" name="no_note" value="0" />
+            <input type="hidden" name="currency_code" value="GBP" />
+            <input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_LG.gif:NonHostedGuest" />
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+          </form>
         </div>
       </div>
     );
